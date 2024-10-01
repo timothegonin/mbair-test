f3fe6e948e9575f7e3e2225faaea09f90db2b955
timotheG <gonin.timothe@gmail.com>
2024-10-01 15:38:38 +0200

    ✏️ fixing typo error eslint confing

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,4 @@
-module.export = {
+module.exports = {
   plugins: ['testing-library', 'jest-dom'],
   extends: ['react-app','react-app/jest','plugin:testing-library/react','plugin:jest-dom/recommended'],
 }
