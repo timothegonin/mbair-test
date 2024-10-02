f405d4c62cc80cbbd75ef037045732f6a9bfee26
timotheG <gonin.timothe@gmail.com>
2024-10-02 19:26:05 +0200

    ✅ products infos test GREEN+ improving tests

--- src/components/ProductInfos.jsx
+++ src/components/ProductInfos.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-// import macbookAir from '../images/macbook-air.jpeg'
+import macbookAir from '../images/macbook-air.jpeg'
 
 const ProductInfos = () => {
   return (
@@ -7,10 +7,10 @@ const ProductInfos = () => {
       <div className="col-md-6 mt-3">
         <div className='bg-white p-3 d-flex flex-column' style={{borderRadius: '14px'}}>
           <div className='text-center mt-4'>
-            {/* <img src={macbookAir} alt="macbookAir" className='img-fluid' /> */}
+            <img src={macbookAir} alt="macbookAir" className='img-fluid' />
           </div>
-          {/* <h5 className='mt-4'>MacBook Air - Gris sidéral</h5> */}
-          {/* <h4 className='green'>1199,00 €</h4> */}
+          <h5 className='mt-4'>MacBook Air - Gris sidéral</h5>
+          <h4 className='green'>1199,00 €</h4>
         </div>
       </div>
     </>
--- src/components/tests/ProductInfos.test.js
+++ src/components/tests/ProductInfos.test.js
@@ -1,12 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import ProductInfos from '../ProductInfos';
 
-test('sould render product informations', () => {
-  render(<ProductInfos />);
-  const productImage = screen.getByAltText('macbookAir');
-  const productTitle = screen.getByRole('heading', {name:"MacBook Air - Gris sidéral", level:5});
-  const productPrice = screen.getByRole('heading', {name:"1199,00 €", level:4});
-  expect(productImage).toBeInTheDocument()
-  expect(productTitle).toBeInTheDocument()
-  expect(productPrice).toBeInTheDocument()
-});
+describe('Product infos', () => {
+  test('sould render the image, title and price', () => {
+    render(<ProductInfos />);
+    const productImage = screen.getByRole('img', {name: /macbookair/i});
+    const productTitle = screen.getByRole('heading', {name:/macbook air - gris sidéral/i, level:5});
+    const productPrice = screen.getByRole('heading', {name:"1199,00 €", level:4});
+    expect(productImage).toBeInTheDocument()
+    expect(productTitle).toBeInTheDocument()
+    expect(productPrice).toBeInTheDocument()
+    expect(productPrice).toHaveClass('green')
+    expect(productPrice).toHaveTextContent('1199,00 €')
+  });
+})
