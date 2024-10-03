fa7ffcd94dbbdb80d42923d07ae000a9dfd921f4
timotheG <gonin.timothe@gmail.com>
2024-10-03 10:16:35 +0200

    ✅ basic Product specs test

--- src/components/ProductInfos.jsx
+++ src/components/ProductInfos.jsx
@@ -7,10 +7,29 @@ const ProductInfos = () => {
       <div className="col-md-6 mt-3">
         <div className='bg-white p-3 d-flex flex-column' style={{borderRadius: '14px'}}>
           <div className='text-center mt-4'>
-            <img src={macbookAir} alt="macbookAir" className='img-fluid' />
+            <img 
+              src={macbookAir}
+              alt="macbookAir"
+              className='img-fluid'
+              width="350"
+            />
           </div>
           <h5 className='mt-4'>MacBook Air - Gris sidéral</h5>
           <h4 className='green'>1199,00 €</h4>
+        </div>
+      </div>
+      <div className="col-md-6 mt-3">
+      <div className='bg-white p-3 d-flex flex-column' style={{borderRadius: '14px'}}>
+          <h3>Personnalisez votre MacBook Air - Gris sidéral</h3>
+          <ul className='list-unstyled'>
+            <li>Puce Apple M1 avec CPU 8 coeurs, Neutral Engine 16 coeurs</li>
+            <li>8 Go de mémoire unifiée</li>
+            <li>SSD de 256 Go</li>
+            <li>Ecran Retina 15 pouces avec True Tone</li>
+            <li>Deux ports Thunderbolt/USB 4</li>
+            <li>Adaptateur USB-C 30 W</li>
+            <li>Magic Keuboard rétroéclairé avec Touch ID - Français</li>
+          </ul>
         </div>
       </div>
     </>
--- src/components/tests/ProductInfos.test.js
+++ src/components/tests/ProductInfos.test.js
@@ -2,8 +2,9 @@ import { render, screen } from '@testing-library/react';
 import ProductInfos from '../ProductInfos';
 
 describe('Product infos', () => {
-  test('sould render the image, title and price', () => {
+  test('sould render the image, title, price and specs', () => {
     render(<ProductInfos />);
+    //PRESENTATION
     const productImage = screen.getByRole('img', {name: /macbookair/i});
     const productTitle = screen.getByRole('heading', {name:/macbook air - gris sidéral/i, level:5});
     const productPrice = screen.getByRole('heading', {name:"1199,00 €", level:4});
@@ -12,5 +13,12 @@ describe('Product infos', () => {
     expect(productPrice).toBeInTheDocument()
     expect(productPrice).toHaveClass('green')
     expect(productPrice).toHaveTextContent('1199,00 €')
+    //CONFIGURATION
+    const helperTitle = screen.getByRole('heading', {level: 3, name:/personnalisez votre macbook air - gris sidéral/i}) 
+    const specsList = screen.getByRole("list")
+    const specsListItems = screen.getAllByRole('listitem')
+    expect(helperTitle).toBeInTheDocument()
+    expect(specsList).toBeInTheDocument()
+    expect(specsListItems.length).toBe(7)
   });
 })
