03f4a8bb0f588b693f213ef964a8b69b9bb2facc
timotheG <gonin.timothe@gmail.com>
2024-10-08 13:57:42 +0200

    🗃️ fetching fake json data

--- src/components/Capacity.jsx
+++ src/components/Capacity.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import CapacityWrapper from './CapacityWrapper'
 
 const Capacity = ({capacityType}) => {
+
+  const [items, setItems] = useState([])
+  console.log(items)
+
+  useEffect(() => {
+    fetch(`http://localhost:3030/${capacityType}`)
+    .then((res) => {return res.json()})
+    .then((data) => {setItems(data)})
+    .catch((error) => console.log(error))
+  }, [capacityType])
+  
+
   return (
     <div>{capacityType === 'ram' ? 
       (<CapacityWrapper capacityType={capacityType}>List RAM capacities</CapacityWrapper> ) : 
