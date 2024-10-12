849ee957045f7e1adc2143b4d0087b62edd79c6b
timotheG <gonin.timothe@gmail.com>
2024-10-12 12:02:35 +0200

    ✅ ssd capacities

--- src/components/tests/Capacity.test.js
+++ src/components/tests/Capacity.test.js
@@ -12,4 +12,16 @@ describe('Checking ram and ssd capacities', () => {
     expect(allRamOptions).toHaveLength(2)
     expect(screen.getByRole("option", {name: "8 Go de mémoire unifiée"}).selected).toBe(true)
   });
+  test('sould render ssd capacities', async () => {
+    render(<Capacity capacityType='ssd'/>);
+    const ssdParagraph = screen.getByText(/sélectionnez la capacité de la stockage/i);
+    const defaultSsdCapacity = await screen.findByRole('radio', {name:"SSD de 256 Go"})
+    const ssdRadioExtensions = await screen.findAllByRole('radio',{name: /.*\u20AC\)/})
+    const allSsdPrices = ssdRadioExtensions.map((input) => input.value)
+    expect(ssdParagraph).toBeInTheDocument()
+    expect(defaultSsdCapacity).toBeInTheDocument()
+    expect(defaultSsdCapacity).toBeChecked()
+    expect(ssdRadioExtensions).toHaveLength(3)
+    expect(allSsdPrices).toEqual(['230', '460', '920'])
+  });
 })
