4de7fa028e1f9b830f0d604537addbb0dd8c2fff
timotheG <gonin.timothe@gmail.com>
2024-10-24 16:16:53 +0200

    ✅ ram capacities update

--- src/components/tests/Configuration.test.js
+++ src/components/tests/Configuration.test.js
@@ -1,6 +1,7 @@
 import { rest } from "msw"
 import { server } from "../../mocks/server"
 import { render, screen, waitFor } from "../../test-utils"
+import userEvent from "@testing-library/user-event"
 import Configuration from "../Configuration"
 import MacBookProviders from "../../providers/MacBookProviders"
 
@@ -25,5 +26,21 @@ test('RAM and SSD errors', async () => {
     const alerts = await screen.findAllByRole('alert')
     expect(alerts).toHaveLength(2)
   })
+})
+test('Updating price and displaying product information based on selected capacities', async () => {
+  const user = userEvent.setup()  
+  render(<Configuration/>)
+  const productPrice = screen.getByRole("heading", {level:4, name: "1199,00 €"})
+  const ramCapacity = screen.getByText('8 Go de mémoire unifiée')
+  const ssdCapacity = screen.getByText('SSD de 256 Go')
 
+  await waitFor(async () => {
+    const allRamOptions = await screen.findAllByRole("option")
+    expect(allRamOptions).toHaveLength(2)
+  })
+
+  const selectElement = screen.getByRole('combobox', {name: /default select/i})
+  await user.selectOptions(selectElement, "230")
+  expect(selectElement.value).toBe('230')
+  expect(screen.getByRole("option", {name: /16 Go de mémoire unifiée/i, exact: false}).selected).toBe(true)
 })
