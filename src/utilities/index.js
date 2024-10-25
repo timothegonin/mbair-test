ba47edf141f87ea6a171580ee3dfeb6ad845ab98
timotheG <gonin.timothe@gmail.com>
2024-10-25 10:16:05 +0200

    ✨ formatPrice function

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -1,3 +1,9 @@
 export const displayPrice = sum => {
   return sum > 0 ? `(${sum} €)` : ""
 }
+
+export function formatPrice(val) {
+  let parts = val.toFixed(2).split(".");
+  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+  return parts.join(",");
+}
