431fc06096dd678f6629c20a7ed169a6f1160824
timotheG <gonin.timothe@gmail.com>
2024-11-22 08:31:03 +0100

    ✅ confirmation step mock

--- src/components/tests/Validation.test.js
+++ src/components/tests/Validation.test.js
@@ -1,5 +1,6 @@
 import { render,screen } from "@testing-library/react"
 import Validation from "../Validation"
+import userEvent from "@testing-library/user-event"
 
 describe('Validaiton component', () => {
   test('Displaying validation button', () => {
@@ -12,4 +13,15 @@ describe('Validaiton component', () => {
     const buttonCart = screen.getByRole('button', {name: /ajouter au panier/i})
     expect(buttonCart).toBeInTheDocument();
   })
+  test('Setter "setStep" used for confirmation', async () => {
+    const user = userEvent.setup()
+    const gotToConfirmationStep = jest.fn()
+    render(<Validation setStep={gotToConfirmationStep} nextLevel="confirmationStep"/>)
+    const buttonConfirmation = screen.getByRole('button', {name: /valider/i})
+    await user.click(buttonConfirmation)
+    expect(gotToConfirmationStep).toHaveBeenCalledTimes(1)
+  })
+  // test('Setter "setStep" used for cart', () => {
+
+  // })
 })
